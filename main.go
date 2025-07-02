2fad64a99cbf7a285bf31cce2678f61a9d49a072
hchao <huachaowu@qq.com>
2025-07-02 21:54:00 +0800

    add consul

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/fs"
 	"log"
 	"mime"
+	"net"
 	"net/http"
 	"os"
 
@@ -15,12 +16,18 @@ import (
 	"bookstore_enhance/third_party"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/hashicorp/consul/api"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/protobuf/encoding/protojson"
+)
+
+const (
+	serviceName string = "bookstore"
+	port        int    = 8888
 )
 
 func main() {
@@ -49,7 +56,7 @@ func run() error {
 		return fmt.Errorf("failed to initialize database: %w", err)
 	}
 
-	addr := "0.0.0.0:8888"
+	addr := fmt.Sprintf(":%d", port)
 	conn, err := grpc.NewClient(
 		"dns:///"+addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -70,6 +77,26 @@ func run() error {
 		return fmt.Errorf("failed to register gRPC gateway: %w", err)
 	}
 
+	// 连接到 consul
+	cc, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		return fmt.Errorf("failed to create Consul client: %w", err)
+	}
+	localIP, err := GetOutboundIP()
+	if err != nil {
+		return fmt.Errorf("failed to get local IP: %w", err)
+	}
+	// 定义我们的服务
+	srv := &api.AgentServiceRegistration{
+		ID:      fmt.Sprintf("%s-%s-%d", serviceName, localIP.String(), port),
+		Name:    serviceName,
+		Tags:    []string{"hchao"},
+		Address: localIP.String(),
+		Port:    port,
+	}
+	// 注册服务到 Consul
+	cc.Agent().ServiceRegister(srv)
+
 	mux := http.NewServeMux()
 	mux.Handle("/", getOpenAPIHandler())
 	mux.Handle(protoconnect.NewBookStoreHandler(&server{bs: &bookstore{db: db}}))
@@ -80,4 +107,15 @@ func run() error {
 	}
 	log.Info("Starting HTTP server on ", addr)
 	return server.ListenAndServe()
+}
+
+// GetOutboundIP 获取本机的出口IP
+func GetOutboundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
 }
